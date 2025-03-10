17c204f3c3daf79e56f2f3f47b2f4234f304be4c
Kittisak Srathong <basbubie.kittisak@gmail.com>
2025-03-11 01:06:41 +0700

    [FIX] hydate mismatch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
+import AboutUs from "@/components/Home/AboutUs";
 import Cover from "@/components/Home/Cover";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
     <>
       <Cover />
+      <AboutUs />
     </>
   );
 }
