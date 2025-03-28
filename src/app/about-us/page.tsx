4132e9bcba89e4448073f3b1445cae8e34fa1c70
Kittisak Srathong <basbubie.kittisak@gmail.com>
2025-03-29 01:31:08 +0700

    [ADD] Highlight Component

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/Footer";
 import Expert from "@/components/AboutUs/Expert";
 import Type from "@/components/AboutUs/Type";
 import TpsTechnology from "@/components/AboutUs/TpsTechnology";
+import Highlights from "@/components/AboutUs/Highlights";
 
 export default function AboutUsPage() {
   const [isClient, setIsClient] = useState(false);
@@ -25,6 +26,7 @@ export default function AboutUsPage() {
       <Expert />
       <Type />
       <TpsTechnology />
+      <Highlights />
       <Footer />
     </>
   );
