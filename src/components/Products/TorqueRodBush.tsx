87745d237bb21509730fca87b01ee90b458388a3
Kittisak Srathong <basbubie.kittisak@gmail.com>
2025-04-16 21:12:34 +0700

    [ADD] Product List of Torque Rod Bush

--- src/components/Products/TorqueRodBush.tsx
+++ src/components/Products/TorqueRodBush.tsx
@@ -1,9 +1,165 @@
+import { Card, Col, Row, Space, Typography } from "antd";
+import styled from "styled-components";
+import Image from "next/image";
 import ProductCover from "@/components/Products/ProductCover";
+import {
+  TORQUE_ROD_BUSH_BOGIE,
+  TORQUE_ROD_BUSH_CHINESE,
+  TORQUE_ROD_BUSH_CRANE,
+  TORQUE_ROD_BUSH_SCANIA,
+} from "@/constant/torque-rod-bush";
+import Link from "next/link";
+
+const { Title } = Typography;
+
+const FullScreenWrapper = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100%;
+  overflow: hidden;
+  padding: 36px 150px;
+  background-color: #ffffff;
+
+  .title {
+    font-size: 3rem;
+  }
+
+  .subtitle {
+    font-size: 1.5rem;
+    font-weight: 400;
+  }
+
+  @media (max-width: 768px) {
+    padding: 36px;
+  }
+`;
+
+const ImageWolf = styled(Image)`
+  height: 100%;
+  width: 100%;
+  border-radius: 12px;
+  object-fit: fill;
+`;
+
+const CardContainer = styled(Card)`
+  height: 220px;
+  width: 220px;
+
+  img {
+    object-fit: contain;
+  }
+`;
+
+const RowContainer = styled(Row)`
+  margin: 24px 0 56px;
+`;
+
+const ColProduct = styled(Col)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonProducts = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  background-color: #ff3130;
+  color: #fff;
+  border-radius: 8px;
+  height: 40px;
+  width: 200px;
+  font-size: 1rem;
+  font-family: var(--font-prompt), sans-serif;
+  margin-top: 12px;
+`;
 
 export default function TorqueRodBush() {
   return (
     <>
       <ProductCover type="torqueRodBush" />
+      <FullScreenWrapper>
+        <Space>
+          <Space direction="vertical" size={4}>
+            <Title level={1} className="title">
+              ลูกหมากเพลาโบกี้
+            </Title>
+            <Title level={2} className="subtitle">
+              TORQUE ROD BUSHING
+            </Title>
+          </Space>
+          <ImageWolf
+            src="/icons/icon-wolf-bigto.png"
+            width={60}
+            height={60}
+            alt=""
+          />
+        </Space>
+        <RowContainer gutter={[24, 32]}>
+          {TORQUE_ROD_BUSH_BOGIE.map((bogie) => (
+            <ColProduct key={bogie.name} xl={6} lg={8} sm={12} xs={24}>
+              <CardContainer
+                hoverable
+                cover={<Image fill alt={bogie.name} src={bogie.image} />}
+              />
+              <Link href={bogie.link} passHref>
+                <ButtonProducts>ดูสินค้า</ButtonProducts>
+              </Link>
+            </ColProduct>
+          ))}
+        </RowContainer>
+        <Title level={1} className="title">
+          ลูกหมาก SCANIA
+        </Title>
+        <RowContainer gutter={[24, 32]}>
+          {TORQUE_ROD_BUSH_SCANIA.map((scania) => (
+            <ColProduct key={scania.name} xl={6} lg={8} sm={12} xs={24}>
+              <CardContainer
+                hoverable
+                cover={<Image fill alt={scania.name} src={scania.image} />}
+              />
+              <Link href={scania.link} passHref>
+                <ButtonProducts>ดูสินค้า</ButtonProducts>
+              </Link>
+            </ColProduct>
+          ))}
+        </RowContainer>
+        <Title level={1} className="title">
+          ลูกหมากรถจีน
+        </Title>
+        <RowContainer gutter={[24, 32]}>
+          {TORQUE_ROD_BUSH_CHINESE.map((chinese) => (
+            <ColProduct key={chinese.name} xl={6} lg={8} sm={12} xs={24}>
+              <CardContainer
+                hoverable
+                cover={<Image fill alt={chinese.name} src={chinese.image} />}
+              />
+              <Link href={chinese.link} passHref>
+                <ButtonProducts>ดูสินค้า</ButtonProducts>
+              </Link>
+            </ColProduct>
+          ))}
+        </RowContainer>
+        <Title level={1} className="title">
+          ลูกหมากเครน
+        </Title>
+        <RowContainer gutter={[24, 32]}>
+          {TORQUE_ROD_BUSH_CRANE.map((crane) => (
+            <ColProduct key={crane.name} xl={6} lg={8} sm={12} xs={24}>
+              <CardContainer
+                hoverable
+                cover={<Image fill alt={crane.name} src={crane.image} />}
+              />
+              <Link href={crane.link} passHref>
+                <ButtonProducts>ดูสินค้า</ButtonProducts>
+              </Link>
+            </ColProduct>
+          ))}
+        </RowContainer>
+      </FullScreenWrapper>
     </>
   );
 }
---
+++ src/constant/torque-rod-bush.ts
@@ -0,0 +1,172 @@
+export const TORQUE_ROD_BUSH_BOGIE = [
+  {
+    name: "bogie-standard",
+    image: "/images/products/torque-rod-bush/bogie/standard/standard-1.jpg",
+    link: "/products/torque-rod-bush/standard",
+  },
+  {
+    name: "bogie-four-holes",
+    image: "/images/products/torque-rod-bush/bogie/four-holes/four-holes-1.jpg",
+    link: "/products/torque-rod-bush/four-holes",
+  },
+  {
+    name: "bogie-wrapper",
+    image: "/images/products/torque-rod-bush/bogie/wrapper/wrapper-1.jpg",
+    link: "/products/torque-rod-bush/wrapper",
+  },
+  {
+    name: "bogie-isuzu",
+    image: "/images/products/torque-rod-bush/bogie/isuzu/isuzu-1.jpg",
+    link: "/products/torque-rod-bush/isuzu",
+  },
+  {
+    name: "bogie-egypt",
+    image: "/images/products/torque-rod-bush/bogie/egypt/egypt-1.jpg",
+    link: "/products/torque-rod-bush/egypt",
+  },
+  {
+    name: "bogie-gx-0",
+    image: "/images/products/torque-rod-bush/bogie/gx-0/gx-0-1.jpg",
+    link: "/products/torque-rod-bush/gx-0",
+  },
+  {
+    name: "bogie-hino-series",
+    image:
+      "/images/products/torque-rod-bush/bogie/hino-series/hino-series-1.jpg",
+    link: "/products/torque-rod-bush/hino-series",
+  },
+  {
+    name: "bogie-lg-mg-mt",
+    image: "/images/products/torque-rod-bush/bogie/lg-mg-mt/lg-mg-mt-1.jpg",
+    link: "/products/torque-rod-bush/lg-mg-mt",
+  },
+  {
+    name: "bogie-p-1",
+    image: "/images/products/torque-rod-bush/bogie/p-1/p-1-1.jpg",
+    link: "/products/torque-rod-bush/p-1",
+  },
+  {
+    name: "bogie-p-2",
+    image: "/images/products/torque-rod-bush/bogie/p-2/p-2-1.jpg",
+    link: "/products/torque-rod-bush/p-2",
+  },
+  {
+    name: "bogie-p-3",
+    image: "/images/products/torque-rod-bush/bogie/p-3/p-3-1.jpg",
+    link: "/products/torque-rod-bush/p-3",
+  },
+  {
+    name: "bogie-p.a.inter",
+    image: "/images/products/torque-rod-bush/bogie/p.a.inter/p.a.inter-1.jpg",
+    link: "/products/torque-rod-bush/p.a.inter",
+  },
+  {
+    name: "bogie-py",
+    image: "/images/products/torque-rod-bush/bogie/py/py-1.jpg",
+    link: "/products/torque-rod-bush/py",
+  },
+  {
+    name: "bogie-sunny",
+    image: "/images/products/torque-rod-bush/bogie/sunny/sunny-1.jpg",
+    link: "/products/torque-rod-bush/sunny",
+  },
+  {
+    name: "bogie-super-black-core",
+    image:
+      "/images/products/torque-rod-bush/bogie/super-black-core/super-black-core-1.jpg",
+    link: "/products/torque-rod-bush/super-black-core",
+  },
+  {
+    name: "bogie-super-open-core",
+    image:
+      "/images/products/torque-rod-bush/bogie/super-open-core/super-open-core-1.jpg",
+    link: "/products/torque-rod-bush/super-open-core",
+  },
+  {
+    name: "bogie-super-small-core",
+    image:
+      "/images/products/torque-rod-bush/bogie/super-small-core/super-small-core-1.jpg",
+    link: "/products/torque-rod-bush/super-small-core",
+  },
+  {
+    name: "bogie-super-pyk",
+    image: "/images/products/torque-rod-bush/bogie/super-pyk/super-pyk-1.jpg",
+    link: "/products/torque-rod-bush/super-pyk",
+  },
+  {
+    name: "bogie-super-ud-outer-head",
+    image:
+      "/images/products/torque-rod-bush/bogie/ud-outer-head/ud-outer-head-1.jpg",
+    link: "/products/torque-rod-bush/ud-outer-head",
+  },
+  {
+    name: "bogie-super-jcm",
+    image: "/images/products/torque-rod-bush/bogie/jcm/jcm-1.jpg",
+    link: "/products/torque-rod-bush/jcm",
+  },
+  {
+    name: "bogie-super-kt",
+    image: "/images/products/torque-rod-bush/bogie/kt/kt-1.jpg",
+    link: "/products/torque-rod-bush/kt",
+  },
+  {
+    name: "bogie-super-tx-68",
+    image: "/images/products/torque-rod-bush/bogie/tx-68/tx-68-1.jpg",
+    link: "/products/torque-rod-bush/tx-68",
+  },
+  {
+    name: "bogie-super-ud",
+    image: "/images/products/torque-rod-bush/bogie/ud/ud-1.jpg",
+    link: "/products/torque-rod-bush/ud",
+  },
+];
+
+export const TORQUE_ROD_BUSH_SCANIA = [
+  {
+    name: "scania-big-hole",
+    image: "/images/products/torque-rod-bush/scania/big-hole/big-hole-1.jpg",
+    link: "/products/torque-rod-bush/big-hole",
+  },
+  {
+    name: "scania-small-hole",
+    image:
+      "/images/products/torque-rod-bush/scania/small-hole/small-hole-1.jpg",
+    link: "/products/torque-rod-bush/small-hole",
+  },
+];
+
+export const TORQUE_ROD_BUSH_CHINESE = [
+  {
+    name: "chinese-tongfong",
+    image: "/images/products/torque-rod-bush/chinese/tongfong/tongfong-1.jpg",
+    link: "/products/torque-rod-bush/tongfong",
+  },
+  {
+    name: "chinese-faw-small",
+    image: "/images/products/torque-rod-bush/chinese/faw-small/faw-small-1.jpg",
+    link: "/products/torque-rod-bush/faw-small",
+  },
+  {
+    name: "chinese-faw-big",
+    image: "/images/products/torque-rod-bush/chinese/faw-big/faw-big-1.jpg",
+    link: "/products/torque-rod-bush/faw-big",
+  },
+  {
+    name: "chinese-faw-dump",
+    image: "/images/products/torque-rod-bush/chinese/faw-dump/faw-dump-1.jpg",
+    link: "/products/torque-rod-bush/faw-dump",
+  },
+  {
+    name: "chinese-pawson",
+    image: "/images/products/torque-rod-bush/chinese/pawson/pawson-1.jpg",
+    link: "/products/torque-rod-bush/pawson",
+  },
+];
+
+export const TORQUE_ROD_BUSH_CRANE = [
+  {
+    name: "crane-tadano",
+    image: "/images/products/torque-rod-bush/crane/tadano/tadano-1.jpg",
+    link: "/products/torque-rod-bush/tadano",
+  },
+];
