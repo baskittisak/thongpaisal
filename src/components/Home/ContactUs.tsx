72027f4150dd1ce43fd830afe1947a4f60e66d72
Kittisak Srathong <basbubie.kittisak@gmail.com>
2025-05-04 20:23:16 +0700

    [UPDATE] Social Equlize

--- src/components/Home/ContactUs.tsx
+++ src/components/Home/ContactUs.tsx
@@ -12,137 +12,159 @@ declare global {
 const { Title } = Typography;
 
 const FullScreenWrapper = styled.div`
-  position: relative;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   overflow: hidden;
   background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   .ant-typography {
     color: #fff !important;
   }
+`;
+
+const RowContainer = styled(Row)`
+  width: 100%;
+  height: 100vh;
 
   @media (max-width: 1200px) {
     height: 100%;
   }
 `;
 
-const RowContainer = styled(Row)`
-  height: 100%;
-`;
-
 const ColContent = styled(Col)<{ color: string }>`
   background-color: ${({ color }) => color};
-  height: 100%;
   text-align: center;
   text-transform: uppercase;
-  padding: 36px 0;
-
-  img {
-    object-fit: cover;
-  }
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   .title {
-    font-size: 6rem;
+    font-size: 5rem;
+    margin: 0;
   }
 
   .subtitle {
-    font-size: 1rem;
+    font-size: 1.25rem;
     font-weight: 400;
   }
 
   .contact {
-    font-size: 2rem;
-    margin: 24px 0 !important;
+    font-size: 1.75rem;
+    margin: 24px 0;
   }
 
-  @media (max-width: 1200px) {
-    height: 140vh;
-
+  @media (max-width: 768px) {
     .title {
-      font-size: 4rem;
+      font-size: 3rem;
+    }
+
+    .contact {
+      font-size: 1.5rem;
     }
   }
 `;
 
-const ImageWrapper = styled.div`
-  height: 100%;
-  position: relative;
-  top: -30px;
+const SocialWrapper = styled.div`
+  width: 100%;
+  max-width: 400px;
+  margin-top: 24px;
 
-  iframe {
+  iframe,
+  .fb-page {
+    width: 100% !important;
     border: none;
-    height: 65%;
-    position: relative;
-    top: 34px;
     border-radius: 16px;
+    overflow: hidden;
   }
 
   .fb_iframe_widget iframe {
     width: 100% !important;
-    height: 100% !important;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
-  img {
-    top: -18px !important;
-    height: 80% !important;
+    height: 500px !important;
   }
 `;
 
 export default function ContactUs() {
   useEffect(() => {
-    if (!window.FB) {
-      const script = document.createElement("script");
-      script.src =
-        "https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v18.0";
-      script.async = true;
-      script.defer = true;
-      script.crossOrigin = "anonymous";
-      script.onload = () => {
-        if (window.FB) {
-          window.FB.XFBML.parse();
-        }
-      };
-      document.body.appendChild(script);
-    } else {
-      window.FB.XFBML.parse();
-    }
+    const loadFacebookSDK = () => {
+      if (!window.FB) {
+        const script = document.createElement("script");
+        script.src =
+          "https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v18.0";
+        script.async = true;
+        script.defer = true;
+        script.crossOrigin = "anonymous";
+        script.onload = () => {
+          if (window.FB) {
+            window.FB.XFBML.parse();
+          }
+        };
+        document.body.appendChild(script);
+      } else {
+        window.FB.XFBML.parse();
+      }
+    };
+
+    loadFacebookSDK();
   }, []);
 
   return (
     <FullScreenWrapper>
-      <RowContainer>
-        <ColContent color="#004aad" xl={8} sm={24} xs={24}>
+      <RowContainer gutter={[0, 16]}>
+        <ColContent color="#004aad" xl={8} md={12} sm={24} xs={24}>
           <Title className="title">FOLLOW</Title>
           <Title className="subtitle">US ON FACEBOOK</Title>
           <Title className="contact">@thongpaisal</Title>
-          <ImageWrapper>
+          <SocialWrapper>
             <div
               className="fb-page"
               data-href="https://www.facebook.com/profile.php?id=61564186742636"
               data-tabs="timeline"
+              data-width="400"
+              data-height="500"
+              data-small-header="false"
+              data-adapt-container-width="true"
+              data-hide-cover="false"
+              data-show-facepile="true"
             />
-          </ImageWrapper>
+          </SocialWrapper>
         </ColContent>
-        <ColContent color="#cc2a48" xl={8} sm={24} xs={24}>
+
+        <ColContent color="#cc2a48" xl={8} md={12} sm={24} xs={24}>
           <Title className="title">FOLLOW</Title>
           <Title className="subtitle">US ON FACEBOOK</Title>
           <Title className="contact">@BIGTO THAI</Title>
-          <div
-            className="fb-page"
-            data-href="https://www.facebook.com/bigtothailand"
-            data-tabs="timeline"
-          />
+          <SocialWrapper>
+            <div
+              className="fb-page"
+              data-href="https://www.facebook.com/bigtothailand"
+              data-tabs="timeline"
+              data-width="400"
+              data-height="500"
+              data-small-header="false"
+              data-adapt-container-width="true"
+              data-hide-cover="false"
+              data-show-facepile="true"
+            />
+          </SocialWrapper>
         </ColContent>
-        <ColContent color="#000000" xl={8} sm={24} xs={24}>
+
+        <ColContent color="#000000" xl={8} md={24} sm={24} xs={24}>
           <Title className="title">FOLLOW</Title>
           <Title className="subtitle">US ON TIKTOK</Title>
           <Title className="contact">@bigtothai</Title>
-          <ImageWrapper>
-            <iframe src="https://www.tiktok.com/embed/@bigtothai" />
-          </ImageWrapper>
+          <SocialWrapper>
+            <iframe
+              src="https://www.tiktok.com/embed/@bigtothai"
+              height="500"
+              allowFullScreen
+            />
+          </SocialWrapper>
         </ColContent>
       </RowContainer>
     </FullScreenWrapper>
