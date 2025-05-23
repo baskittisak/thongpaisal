e1ee4d8fe682d241d6e704900787d714d5ca3677
Kittisak Srathong <basbubie.kittisak@gmail.com>
2025-05-23 21:16:23 +0700

    [EDIT] Style Detail

--- src/components/Home/Details.tsx
+++ src/components/Home/Details.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Typography } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import styled from "styled-components";
 import Image from "next/image";
 
@@ -7,10 +7,15 @@ const { Title } = Typography;
 const FullScreenWrapper = styled.div`
   position: relative;
   width: 100vw;
-  height: 100vh;
+  height: 80vh;
   overflow: hidden;
   padding: 36px 200px;
   background-color: #f5f4f3;
+
+  .ant-card-body {
+    overflow: hidden;
+    padding: 12px;
+  }
 
   @media (max-width: 1200px) {
     height: 100%;
@@ -52,60 +57,75 @@ export default function Details() {
     <FullScreenWrapper>
       <RowContainer gutter={[32, 32]}>
         <ColContent xl={8} lg={12} sm={24} xs={24}>
-          <Image
-            width={320}
-            height={230}
-            alt="thongpaisal"
-            src="/images/image-home-details-1.png"
-          />
-          <Text>
-            <Title level={2}>เราพัฒนาสินค้า</Title>
-            <Title level={5}>
-              ลูกหมากรถบรรทุก BIGTO เป็นลูกหมากหุ้มผ้าใบรายแรกในประเทศไทย
-              <br />
-              ที่คิดค้นมาเพื่อเพิ่มประสิทธิภาพในการใช้งาน
-              <br />
-              ให้มีระยะเวลาในการใช้งานได้นานขึ้น
-              <br />
-              ได้รับความไว้วางใจจากเจ้าของธุรกิจขนส่งสินค้าโลจิสติกส์และทรานสปอร์ต
-            </Title>
-          </Text>
+          <Card
+            cover={
+              <Image
+                width={450}
+                height={450}
+                alt="thongpaisal"
+                src="/images/image-home-details-1.png"
+              />
+            }
+          >
+            <Text>
+              <Title level={2}>เราพัฒนาสินค้า</Title>
+              <Title level={5}>
+                ลูกหมากรถบรรทุก BIGTO เป็นลูกหมากหุ้มผ้าใบรายแรกในประเทศไทย
+                <br />
+                ที่คิดค้นมาเพื่อเพิ่มประสิทธิภาพในการใช้งาน
+                <br />
+                ให้มีระยะเวลาในการใช้งานได้นานขึ้น
+                <br />
+                ได้รับความไว้วางใจจากเจ้าของธุรกิจขนส่งสินค้าโลจิสติกส์และทรานสปอร์ต
+              </Title>
+            </Text>
+          </Card>
         </ColContent>
         <ColContent xl={8} lg={12} sm={24} xs={24}>
-          <Image
-            width={320}
-            height={230}
-            alt="thongpaisal"
-            src="/images/image-home-details-2.png"
-          />
-          <Text>
-            <Title level={2}>ใส่ใจวัตถุดิบ</Title>
-            <Title level={5}>
-              วัสดุที่ใช้ผลิตอะไหล่ ผ่านการคัดสรรคุณภาพ
-              และผ่านการทดสอบคุณภาพความแข็งแรงปลอดภัยเพื่อพร้อม
-              <br />
-              เรามีสวนยางพาราเองที่ใช้ผลิตงานยางตั้งเเต่ต้นสาย
-              <br />
-              จึงควบคุมคุณภาพได้ดีเเละได้รับมาตราฐาน
-            </Title>
-          </Text>
+          <Card
+            cover={
+              <Image
+                width={450}
+                height={450}
+                alt="thongpaisal"
+                src="/images/image-home-details-2.png"
+              />
+            }
+          >
+            <Text>
+              <Title level={2}>ใส่ใจวัตถุดิบ</Title>
+              <Title level={5}>
+                วัสดุที่ใช้ผลิตอะไหล่ ผ่านการคัดสรรคุณภาพ
+                และผ่านการทดสอบคุณภาพความแข็งแรงปลอดภัยเพื่อพร้อม
+                <br />
+                เรามีสวนยางพาราเองที่ใช้ผลิตงานยางตั้งเเต่ต้นสาย
+                <br />
+                จึงควบคุมคุณภาพได้ดีเเละได้รับมาตราฐาน
+              </Title>
+            </Text>
+          </Card>
         </ColContent>
         <ColContent xl={8} lg={12} sm={24} xs={24}>
-          <Image
-            width={320}
-            height={230}
-            alt="thongpaisal"
-            src="/images/image-home-details-3.png"
-          />
-          <Text>
-            <Title level={2}>ขนาดมาตรฐาน</Title>
-            <Title level={5}>
-              การใช้เครื่องมือที่มีประสิทธิภาพ เนื่องจากเครื่องกลึง CNC
-              <br />
-              มีการตรวจวัดระยะและควบคุมการทำงาน
-              <br /> ด้วยคอมพิวเตอร์ ทำให้มีความละเอียดสูงกว่าการทำด้วยมือ
-            </Title>
-          </Text>
+          <Card
+            cover={
+              <Image
+                width={450}
+                height={450}
+                alt="thongpaisal"
+                src="/images/image-home-details-3.png"
+              />
+            }
+          >
+            <Text>
+              <Title level={2}>ขนาดมาตรฐาน</Title>
+              <Title level={5}>
+                การใช้เครื่องมือที่มีประสิทธิภาพ เนื่องจากเครื่องกลึง CNC
+                <br />
+                มีการตรวจวัดระยะและควบคุมการทำงาน
+                <br /> ด้วยคอมพิวเตอร์ ทำให้มีความละเอียดสูงกว่าการทำด้วยมือ
+              </Title>
+            </Text>
+          </Card>
         </ColContent>
       </RowContainer>
     </FullScreenWrapper>
